788c5bfb31e092e6cbfcbaca8962cb522c4b1ea5
Michael Khaikin <michael.khaikin@gmail.com>
2020-12-23 21:17:06 +0000

    mongoose connection and personSchema

--- myApp.js
+++ myApp.js
@@ -1,7 +1,18 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const { Schema } = mongoose;
 
-let Person;
+const personSchema = new Schema({
+	name : {type:String, required: true},
+	age : {type : Number},
+	favoriteFoods : {type: [String]}
+});
+
+let Person = mongoose.model('Person', personSchema);
+
+let person = new Person();
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
